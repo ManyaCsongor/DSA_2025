955eb04aafc1c9a6c2e5552dde981b3f7b503099
ManyaCsongor <manyacsongor@gmail.com>
2025-02-25 16:58:38 +0200

    commit lab1

--- labs/lab_01/main.c
+++ labs/lab_01/main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
-int main(void) {
-    printf("asd!\n");
+#include "functions.h"
+
+int main() {
+    int n;
+    scanf("%d",&n);
+    float* array=allocateMemoryForArray1(n);
+    fillWithRandomNumbers(n,array,0,1);
+    printArray(n,array,"print.txt");
+    printf("%.2f", minimum(array[0],array[n-1],array[n/2]));
+    deallocateMemoryForArray(&array);
     return 0;
 }
