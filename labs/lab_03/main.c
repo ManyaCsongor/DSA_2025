5835233a347941b7b3222f6989db06d4a5d5e595
ManyaCsongor <manyacsongor@gmail.com>
2025-03-04 18:10:11 +0200

    commit lab3

--- labs/lab_03/main.c
+++ labs/lab_03/main.c
@@ -1,6 +1,10 @@
+#include "Student.h"
 #include <stdio.h>
 
+
 int main(void) {
-    printf("Hello, World!\n");
+    //printf("%i", sum(10,2));
+    Student_t student1={"Manya Csongor","IYRAZN","Marosvasarhely",{2005,07,06},0, 9.22};
+    printStudent(student1);
     return 0;
 }
